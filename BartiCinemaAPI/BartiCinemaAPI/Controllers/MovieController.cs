9f1731f5cb91ca3fa1459097a25f7249c3f32be9
nowak.b <nowak.b@e80group.com>
2022-03-23 15:45:21 +0100

    Add BartiCinemaAPI Project Add MovieController

---
+++ BartiCinemaAPI/BartiCinemaAPI/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using BartiCinemaDataAccessADO.DataAccess;
+using BartiCinemaDataAccessADO.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BartiCinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieData _movieData;
+
+        public MovieController(IMovieData movieData)
+        {
+            _movieData = movieData;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieDAL>>> Get(int? id, string title, string director)
+        {
+            return await _movieData.GetMovies(id, title, director);
+        }
+
+
+    }
+}
